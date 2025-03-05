e674ec643baa2de909fac99479be02078586c2e0
TuKankamon <antovedaros@gmail.com>
2025-03-05 10:26:37 +0100

    reset

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,6 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
