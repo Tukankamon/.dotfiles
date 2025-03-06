c6ef4d3e5b519f3d1dd80a2529671124c3de8581
TuKankamon <antovedaros@gmail.com>
2025-03-06 10:40:34 +0100

    kept systemd

--- configuration.nix
+++ configuration.nix
@@ -21,13 +21,17 @@
 
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";     #only works in vm apparently
-  boot.loader.grub.useOSProber = true;
 
-  /*boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;*/
+  /*boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";     #only works in vm apparently
+  boot.loader.grub.useOSProber = true;*/
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.plymouth.enable = true;
 
   #start-point
 
