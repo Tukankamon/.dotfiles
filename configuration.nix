f29e94f847f5b6a046e5fc844646e946c647f330
TuKankamon <antovedaros@gmail.com>
2025-03-10 22:11:48 +0100

    idk3

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,12 @@
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };  
 
+programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+
 xdg.portal = {			#This and nix settings is for hyprland
 	enable = true;
 	extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
