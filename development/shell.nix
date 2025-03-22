8840a6139d5bf221b7f6cb6d2eee182b0593d3ba
TuKankamon <antovedaros@gmail.com>
2025-03-22 20:16:16 +0100

    shell

--- development/shell.nix
+++ development/shell.nix
@@ -3,12 +3,13 @@
 { pkgs ? import <nixpkgs> {} }: 
 
 pkgs.mkShell {
-  
+  #General nix shell
   packages = [
     (pkgs.python3.withPackages(pypkgs: with pypkgs; [
       #All the python libraries
       numpy
       requests
+      manim
     ]))
   ];
 
