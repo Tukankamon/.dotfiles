2cfabcd7bd9f722e6ac3ec6338feac0d9e7b5dbb
TuKankamon <antovedaros@gmail.com>
2025-04-02 09:06:30 +0200

    formatt

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -1,27 +1,34 @@
-{ inputs, pkgs, configuration, ... }:	#add inputs
+{
+  inputs,
+  pkgs,
+  configuration,
+  ...
+}: # add inputs
 
 {
-  imports =
-    [ 
-      ./hardware-configuration.nix
-      ./../konfig/home/nvf.nix
-      inputs.home-manager.nixosModules.home-manager
-      ./../konfig/system/laptop.nix
-      ./../konfig/system/global.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    ./../konfig/home/nvf.nix
+    inputs.home-manager.nixosModules.home-manager
+    ./../konfig/system/laptop.nix
+    ./../konfig/system/global.nix
+  ];
 
   laptop-boot.enable = true;
 
-  home-manager = {        #Not necesary but now hm also rebuilds with nixos-rebuild
+  home-manager = {
+    # Not necesary but now hm also rebuilds with nixos-rebuild
     extraSpecialArgs = { inherit inputs; };
     users.aved = import ./home.nix;
 
-  };   
+  };
 
- /*nix.settings = {       #This has to be enabled before installing hyprland
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  };  */
+  /*
+    nix.settings = {       #This has to be enabled before installing hyprland
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+  */
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -36,8 +43,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  /*services.xserver.wacom.enable = true;  #Wacom tablet
-  hardware.opentabletdriver.enable = true;    */
+  /*
+    services.xserver.wacom.enable = true;  #Wacom tablet
+    hardware.opentabletdriver.enable = true;
+  */
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -55,7 +64,6 @@
   #hardware.pulseaudio.enable = true;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-
 
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -75,33 +83,38 @@
   users.users.aved = {
     isNormalUser = true;
     description = "aved";
-    extraGroups = [ "networkmanager" "wheel" ];
-    password = "aved";
-    packages = with pkgs; [
-    #  thunderbird
+    extraGroups = [
+      "networkmanager"
+      "wheel"
     ];
+    password = "aved";
+    /*
+      packages = with pkgs; [
+      #  thunderbird
+      ];
+    */
   };
 
   programs.firefox.enable = false;
-  
-  environment.shells = with pkgs; [ fish];	#Following a video
+
+  environment.shells = with pkgs; [ fish ]; # Following a video
   users.defaultUserShell = pkgs.fish;
   programs.fish.enable = true;
 
   environment.systemPackages = with pkgs; [
-   gnomeExtensions.blur-my-shell
-   gnomeExtensions.tiling-shell
-   #gnomeExtensions.search-light
-   gimp
-   sops
-  #  wget
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.tiling-shell
+    #gnomeExtensions.search-light
+    gimp
+    sops
+    #  wget
   ];
 
   fonts.packages = with pkgs; [
     font-awesome
     font-awesome_5
     font-awesome_6
-    
+
     nerd-fonts.code-new-roman
     pkgs.nerd-fonts.jetbrains-mono
     pkgs.nerd-fonts.fira-code
@@ -131,68 +144,71 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
- 
+
   services.power-profiles-daemon.enable = false;
-  
-  powerManagement.enable = true;      #laptop battery
+
+  powerManagement.enable = true; # laptop battery
   services.thermald.enable = true;
 
- services.tlp = {
-  enable = true;
-  settings= {
-    CPU_SCALING_GOVERNOR_ON_AC = "performance";
-    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-    CPU_ENERGY_PERF_POLICY_ON_BAT= "power";
-    CPU_ENERGY_PERF_POLICY_ON_AC= "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-    CPU_MIN_PERF_ON_AC = 0;
-    CPU_MAX_PERF_ON_AC = 100;
-    CPU_MIN_PERF_ON_BAT = 0;
-    CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
 
-  };
- };
-
- nix.gc = {
-  automatic = true;
-  dates = "weekly";
-  options = "--delete-older-than 10d";
- };
-
-/*services.libinput = {
-  enable = true;
-  # disabling mouse acceleration
-  mouse = {
-    accelProfile = "flat";
+    };
   };
 
-  # disabling touchpad acceleration
-  touchpad = {
-    accelProfile = "flat";
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 10d";
   };
-  };
+
+  /*
+    services.libinput = {
+    enable = true;
+    # disabling mouse acceleration
+    mouse = {
+      accelProfile = "flat";
+    };
+
+    # disabling touchpad acceleration
+    touchpad = {
+      accelProfile = "flat";
+    };
+    };
   */
- nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
 }
- 
- /*stylix.base16Scheme = {
-  base00 = "1f262d";  #terminal bg (defualt is #485867) (brush trees dark)
-  base01 = "5A6D7A";
-  base02 = "6D828E";
-  base03 = "8299A1";
-  base04 = "98AFB5";
-  base05 = "B0C5C8";
-  base06 = "C9DBDC";
-  base07 = "E3EFEF";
-  base08 = "b38686";
-  base09 = "d8bba2";
-  base0A = "aab386";
-  base0B = "87b386";
-  base0C = "86b3b3";
-  base0D = "868cb3";
-  base0E = "b386b2";
-  base0F = "b39f9f";
 
- };*/
+/*
+  stylix.base16Scheme = {
+   base00 = "1f262d";  #terminal bg (defualt is #485867) (brush trees dark)
+   base01 = "5A6D7A";
+   base02 = "6D828E";
+   base03 = "8299A1";
+   base04 = "98AFB5";
+   base05 = "B0C5C8";
+   base06 = "C9DBDC";
+   base07 = "E3EFEF";
+   base08 = "b38686";
+   base09 = "d8bba2";
+   base0A = "aab386";
+   base0B = "87b386";
+   base0C = "86b3b3";
+   base0D = "868cb3";
+   base0E = "b386b2";
+   base0F = "b39f9f";
+
+  };
+*/
--- laptop/flake.nix
+++ laptop/flake.nix
@@ -5,55 +5,70 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";	#release-24.11 (syntax for stable branch)
-      inputs.nixpkgs.follows = "nixpkgs";	#Check if the versions are the same
+      url = "github:nix-community/home-manager/master"; # release-24.11 (syntax for stable branch)
+      inputs.nixpkgs.follows = "nixpkgs"; # Check if the versions are the same
     };
 
-    nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    /*
+      nvf = {
+        url = "github:notashelf/nvf";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
+    */
 
-    /*hyprland = {
+    /*
+      hyprland = {
       url = "github:hyprwm/Hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
-      };*/
-    
+      };
+    */
+
   };
 
-  outputs = { self, nixpkgs, home-manager, nvf, /*hyprland,*/ ... }@inputs:   #If not inside the curly brackets you need to put inputs. e.g: inputs.nixpkgs...
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager, # hyprland,
+      ...
+    }@inputs: # If not inside the curly brackets you need to put inputs. e.g: inputs.nixpkgs...
     let
       lib = nixpkgs.lib;
-      system = "x86_64-linux";      
+      system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      
-    in {
 
-    nixosConfigurations = {
-      nixos = lib.nixosSystem {
-        specialArgs = {inherit inputs system;};
-    	  modules = [ ./configuration.nix
-		      nvf.nixosModules.default
-		      ];
+    in
+    {
+
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          specialArgs = { inherit inputs system; };
+          modules = [
+            ./configuration.nix
+            # nvf.nixosModules.default
+          ];
+        };
       };
-    };
-    
-    homeConfigurations = {
-      aved = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-    	  modules = [ ./home.nix 
-          /*{
-          wayland.windowManager.hyprland = {
-            enable = true;
-            # set the flake package
-            package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-            portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-          };
-          } */
-        
-        ];
+
+      homeConfigurations = {
+        aved = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
+            /*
+              {
+              wayland.windowManager.hyprland = {
+                enable = true;
+                # set the flake package
+                package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+                portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+              };
+              }
+            */
+
+          ];
+        };
       };
+
     };
-	
-  };
 }
