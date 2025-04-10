16406fa8f6cb2f76959b8345af80e4b46b63e6af
TuKankamon <antovedaros@gmail.com>
2025-04-10 13:24:41 +0200

    script

--- pc/configuration.nix
+++ pc/configuration.nix
@@ -121,9 +121,9 @@
     #deadnix  #Scan for unused nix code   (https://github.com/astro/deadnix)
     nix-output-monitor  #eye candy for nix develop and shell
     (writeShellScriptBin "deploy" ''
-      nohup brave /dev/null 2>&1&
-      nohup codium /dev/null 2>&1&
-      nohup discord /dev/null 2>&1&
+      nohup brave &
+      nohup codium &
+      nohup discord &
     '') # Custom bash script to open programs
     libreoffice
     audacity
