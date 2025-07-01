2fd4dba5b5cc9c3ba97069dec293168012245f1d
TuKankamon <antovedaros@gmail.com>
2025-07-01 09:31:09 +0200

    idk

--- other/theme/cursor/extract.sh
+++ other/theme/cursor/extract.sh
@@ -1,4 +1,4 @@
 # Just doing this now so I dont forget, doesnt do much yet, you run this command to set the icons into the directory
 # Need to make a for loop or something
 
-tar xvf foobar-cursor-theme.tar.gz -C ~/.local/share/icons
+tar xvf Future-cyan-cursors.tar.gz -C ~/.local/share/icons
