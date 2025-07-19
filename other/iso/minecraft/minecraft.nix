22aa3e0d69ba31329e0b8908a450f4ac4f447853
TuKankamon <antovedaros@gmail.com>
2025-07-19 19:10:39 +0200

    version

--- other/iso/minecraft/minecraft.nix
+++ other/iso/minecraft/minecraft.nix
@@ -5,12 +5,13 @@
         enable = true;
         eula = true;
         declarative = true;
-        package = pkgs.minecraft-server-1-16;
+        package = pkgs.minecraft-server_1_16;
 
         serverProperties = {
           gamemode = "survival";
           difficulty = "hard";
           simulation-distance = 10;  #I think this is chunk render distance
+          motd = "Minecraft TuKa";
         };
   };
 }
