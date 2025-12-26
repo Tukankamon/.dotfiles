6a52a690cba1c9f0820ac33f311cf9fd0a9e009f
TuKankamon <198082906+Tukankamon@users.noreply.github.com>
2025-12-26 12:07:12 +0100

    different password on laptop

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -62,7 +62,7 @@
             #"input"
         ];
 
-        hashedPassword = "$y$j9T$V8fcqlSY6CKWRjX6fsffR.$Im1Sttp46CQIw9J/ZIf5x6LdaUziJxxLf5hyub8gIv1";
+        hashedPassword = "$y$j9T$b8y4kvjxHbKJ2eyDRBMVJ.$gwOzHjB/WqPidnWWRL7qZKwYEEhzHZn2uraUu3f5f0/";
 
         packages = with pkgs; [
             #  thunderbird
