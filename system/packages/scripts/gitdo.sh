1ede1ab104b69907f7a2f7e24ee24b6e9b832a4e
TuKankamon <198082906+Tukankamon@users.noreply.github.com>
2026-01-21 15:35:22 +0100

    idk

--- system/packages/scripts/gitdo.sh
+++ system/packages/scripts/gitdo.sh
@@ -5,7 +5,7 @@ Help()
   # Display Help
   echo "Runs git add, commit and push and prompts you for a git message"
   echo
-  echo "-m to add a commit message inline"
+    echo "-m to add a commit message inline (use quotes for multiword inputs)"
   echo "-p to pull before pushing"
   echo "-r to change the remote (default is main)"
   echo "-h for help"
